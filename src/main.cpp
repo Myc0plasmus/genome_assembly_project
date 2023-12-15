caab5fe20acb06f524d71aace68d6e09ccd78086
Myc0plasmus <jablonski.natan@gmail.com>
2023-12-15 01:02:35 +0100

    added levenshtein

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,52 @@
 
 using namespace std;
 
+int levenshteinFullMatrix(const string& str1,
+                        const string& str2)
+{
+    int m = str1.length();
+    int n = str2.length();
+ 
+    vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
+ 
+    for (int i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+ 
+    for (int j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+ 
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else {
+                dp[i][j] = 1
+                        + min(
+ 
+                            // Insert
+                            dp[i][j - 1],
+                            min(
+ 
+                                // Remove
+                                dp[i - 1][j],
+ 
+                                // Replace
+                                dp[i - 1][j - 1]));
+            }
+        }
+    }
+ 
+    return dp[m][n];
+}
+
 int main()
 {
 	srand(time(0));
 	// Sequence a = Sequence("GCTCGGCTACATGATCCTTACCACCACCGAGTTCACACGATGTCGATAGAAATACGCGCAGATCTTTTGCGCACTGTAGCCGCGATTCCGCCAGTTTCAC");
-	Sequence a = Sequence(100);
+	Sequence a = Sequence(1000);
 	cout<<"Seqence: "<<endl<<a.seq<<endl;
 	a.shredSequence({});
 	a.createDefaultGraph();
@@ -50,6 +91,7 @@ int main()
 	cout<<"Seqence: "<<endl<<a.seq<<endl;
 	cout<<"results:"<<endl;
 	for(auto it : paths ){
+		cout<<"solution distance: "<<levenshteinFullMatrix(a.seq,it.second)<<endl;
 		cout<<it.first<<"\t"<<it.second<<endl;
 	}
 }
